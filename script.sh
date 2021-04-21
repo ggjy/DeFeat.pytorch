c46a793df414a42f64f9fad9c7106ee60b44c9b3
Nevermore <348797244@qq.com>
2021-04-21 21:29:33 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 # VOC: train ResNet-50 based Faster RCNN 
-python -m torch.distributed.launch --nproc_per_node=1 tools/train.py --gpus 1 --launcher pytorch --validate --work-dir /tmp/coco_output/kd/r50/ --config configs/faster_rcnn/voc_faster_rcnn_r50.py
+python -m torch.distributed.launch --nproc_per_node=2 tools/train.py --gpus 2 --launcher pytorch --validate --work-dir /tmp/coco_output/kd/r50/ --config configs/faster_rcnn/voc_faster_rcnn_r50.py
 
 # VOC: train ResNet-101 based Faster RCNN 
-python -m torch.distributed.launch --nproc_per_node=1 tools/train.py --gpus 1 --launcher pytorch --validate --work-dir /tmp/coco_output/kd/r101/ --config configs/faster_rcnn/voc_faster_rcnn_r101.py
+python -m torch.distributed.launch --nproc_per_node=2 tools/train.py --gpus 2 --launcher pytorch --validate --work-dir /tmp/coco_output/kd/r101/ --config configs/faster_rcnn/voc_faster_rcnn_r101.py
 
 # KD FGFI: train student 
-python -m torch.distributed.launch --nproc_per_node=1 tools/train_kd.py --gpus 1 --launcher pytorch --validate --work-dir /tmp/coco_output/kd/r101-50-FGFI/ --config configs/kd_faster_rcnn/voc_stu_faster_rcnn_r50_FGFI.py --config-t configs/kd_faster_rcnn/voc_tea_faster_rcnn_r101.py --checkpoint-t /tmp/coco_output/voc_bn_r101_8x2_lr0.02/epoch_4.pth
+python -m torch.distributed.launch --nproc_per_node=2 tools/train_kd.py --gpus 2 --launcher pytorch --validate --work-dir /tmp/coco_output/kd/r101-50-FGFI/ --config configs/kd_faster_rcnn/voc_stu_faster_rcnn_r50_FGFI.py --config-t configs/kd_faster_rcnn/voc_tea_faster_rcnn_r101.py --checkpoint-t /tmp/coco_output/voc_bn_r101_8x2_lr0.02/epoch_4.pth
 
 # KD DeFeat: train student 
-To be added.
+python -m torch.distributed.launch --nproc_per_node=2 tools/train_kd.py --gpus 2 --launcher pytorch --validate --work-dir /tmp/coco_output/kd/r101-50-DeFeat/ --config configs/kd_faster_rcnn/voc_stu_faster_rcnn_r50_decouple_neck.py --config-t configs/kd_faster_rcnn/voc_tea_faster_rcnn_r101.py --checkpoint-t /tmp/coco_output/voc_bn_r101_8x2_lr0.02/epoch_4.pth
